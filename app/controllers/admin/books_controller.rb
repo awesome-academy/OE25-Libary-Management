1f37412385e630dc11f1db3a9b98134b8933bdfa
Hoa-Huong <hoahuongad@gmail.com>
2019-11-14 14:11:02 +0700

    Edit book

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,3 +1,25 @@
 class Admin::BooksController < AdminController
   include BookAction
+
+  before_action :find_book, only: %i(show edit update)
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @book.update book_params
+      flash.now[:success] = t "edit_success"
+      redirect_to [:admin, @book]
+    else
+      flash.now[:danger] = t "edit_fail"
+      render :edit
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit Book::BOOK_PARAMS
+  end
 end
