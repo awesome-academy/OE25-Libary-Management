fb373bac891ffd8b73e2315a116ac11164ba65cc
MenBui <menbui120198@gmail.com>
2019-12-17 09:57:00 +0700

    edit_author

--- app/controllers/admin/authors_controller.rb
+++ app/controllers/admin/authors_controller.rb
@@ -1,7 +1,9 @@
 class Admin::AuthorsController < AdminController
-  before_action :load_author, only: %i(show destroy)
+  before_action :load_author, except: %i(index)
 
   def show; end
+
+  def edit; end
 
   def index
     @authors = Author.page(params[:page]).per Settings.page_book
@@ -23,7 +25,21 @@ class Admin::AuthorsController < AdminController
     end
   end
 
+  def update
+    if @author.update author_params
+      flash[:success] = t "author_update"
+      redirect_to admin_author_path
+    else
+      flash[:danger] = t "update_false"
+      render :edit
+    end
+  end
+
   private
+
+  def author_params
+    params.require(:author).permit Author::AUTHOR_PARAMS
+  end
 
   def load_author
     return if @author = Author.find_by(id: params[:id])
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,7 @@
 class Author < ApplicationRecord
+  AUTHOR_PARAMS = %i(name description).freeze
   has_many :books, dependent: :destroy
 
   validates :name, presence: true, length: {maximum: Settings.max_name_author}
-  validates :decription, length: {maximum: Settings.max_decription_author}
+  validates :description, length: {maximum: Settings.max_description_author}
 end
